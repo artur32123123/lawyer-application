8eab7f6f3afd30e5b8fb6894150dce68ebaa8502
Артур <ffilosssoff@gmail.com>
2025-01-31 12:24:20 +0300

    FIX Изменил верстку в register

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -20,8 +20,39 @@
     </div>
     <div class="auth">
         <ul class="register">
-            <li><a href="#">Авторизация</a></li>
-            <li><a href="{{ asset('register') }}">Регистрация</a></li>
+            @guest
+                @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Авторизация') }}</a>
+                    </li>
+                @endif
+
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Регистрация') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+
+
         </ul>
     </div>
 </header>
