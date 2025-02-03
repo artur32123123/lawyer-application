622b4e4d76a9bc34a71c309493dfcc3c240b4f7d
Артур <ffilosssoff@gmail.com>
2025-02-03 22:39:41 +0300

    FIX Доработка swiper

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,31 +3,37 @@
 <html lang="en">
 
     <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
-
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+  />
     <body>
-        <s defer src="{{ asset('js/swiper.js') }}"></s cript>
+        <script defer src="{{ asset('js/swiper.js') }}"></script>
 
     <x-header />
     <section class="motorbike container">
-        <div class="motorbike-card">
+        <div class="motorbike-card ">
             <div class="swiper mySwiperClass">
                 <div class="swiper-wrapper">
-                    <!-- Slidess -->
-                    @foreach ($post->post_images as $key => $image)
-                        <div class="swiper-slide">
-                            <div class="motorbike-card__img">
-                                <img src="{{ $image->src }}" alt="motorbike">
+                    @if ($post->post_images->count() > 0)
+                        @foreach ($post->post_images as $key => $image)
+                            <div class="swiper-slide">
+                                <div class="motorbike-card__img">
+                                    <img src="{{ $image->src }}" alt="motorbike">
+                                </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    @else
+                        <p>No images found.</p>
+                    @endif
                 </div>
-
                 <!-- Pagination -->
                 <div class="swiper-pagination"></div>
                 <!-- Controls -->
                 <div class="swiper-button-prev"></div>
                 <div class="swiper-button-next"></div>
             </div>
+
             <div class="motorbike-card__info">
                 <h2 class="desck">Motorbike Information</h2>
                 <p class="desck">Model: {{ $post->title }}</p>
@@ -37,6 +43,8 @@
             </div>
         </div>
     </section>
+    <!-- Slider main container -->
+
     <script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
